36c3090ffa55a0c59198305a6ed110a08a0aef70
Jason_Shu <shuyue524@gmail.com>
2026-01-20 22:51:31 +0800

    feature: add ErrorPage

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../layouts/MainLayout";
 import { welcomeRoutes } from "./welcomeRoutes";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <NotFoundPage />,
     children: [welcomeRoutes],
   },
 ]);
